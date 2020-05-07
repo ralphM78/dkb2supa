e3d1cb861768133521feb7859be9207f79660f4c
ralphM78 <github-ralph@schosemail.de>
2020-05-07 07:44:17 +0200

    check for 'line starts with dates' and some comments

--- dkb2supa.py
+++ dkb2supa.py
@@ -32,9 +32,10 @@
 import argparse, csv, os
 from sys import exit
 from datetime import datetime
+from re import match
 from schwifty import IBAN
 
-parser = argparse.ArgumentParser(description='Convert DKB CSV into SUPA CSV.')
+parser = argparse.ArgumentParser(description="Convert DKB CSV into SUPA CSV.")
 parser.add_argument("-i", "--input", help="DKB CSV input file", type=argparse.FileType("r", encoding="iso-8859-1"), required=True, metavar="INPUTFILE")
 parser.add_argument("-o", "--output", help="SUPA formated CSV output file", type=argparse.FileType("w", encoding="utf-8"), required=True, metavar="OUTPUTFILE")
 parser.add_argument("--iban", help="Optionally your IBAN", metavar="DE820000000000")
@@ -47,6 +48,7 @@ OwnrAcctBankCode = ""
 OwnrAcctBIC = ""
 OwnrAcctNo = ""
 
+# check for valid IBAN
 if args.iban:
     try:
         OwnrAcctIBAN = IBAN(args.iban)
@@ -55,12 +57,14 @@ if args.iban:
         print("ERROR in --iban argument: ", error_string)
         exit(1)
 
+# check for currency length if passed
 if args.cur:
     cur = args.cur.upper()
     if len(cur) != 3:
         print ("Currency can have a length of 3 chars only. EUR,USD,CHF ...")
         exit(1)
 
+# convert IBAN if passed
 if OwnrAcctIBAN:
     OwnrAcctIBANformated = OwnrAcctIBAN.formatted
     OwnrAcctBIC = str(OwnrAcctIBAN.bic)
@@ -82,26 +86,31 @@ with args.input as CsvInputfile:
 
     with args.output as CsvOutputfile:
         outputData = csv.writer(CsvOutputfile, delimiter=',')
+        # writing header line
         outputData.writerow(["Id", "AcctId", "OwnrAcctCcy", "OwnrAcctIBAN", "OwnrAcctNo", "OwnrAcctBIC", "OwnrAcctBankCode", "BookgDt", "ValDt", "TxDt", "Amt", "AmtCcy", "CdtDbtInd", "EndToEndId", "PmtInfId", "MndtId", "CdtrId", "RmtInf", "PurpCd", "BookgTxt", "PrimaNotaNo", "BankRef", "BkTxCd", "RmtdNm", "RmtdUltmtNm", "RmtdAcctCtry", "RmtdAcctIBAN", "RmtdAcctNo", "RmtdAcctBIC", "RmtdAcctBankCode", "BookgSts", "BtchBookg", "BtchId", "GVC", "GVCExtension", "Category", "Notes", "ReadStatus", "Flag"])
 
         bookings = 0
 
         for row in inputData:
-            # convert booking date
-            BookDt = datetime.strptime(row[0], "%d.%m.%Y").strftime("%Y-%m-%d")
-            # convert value date
-            ValDt = datetime.strptime(row[1], "%d.%m.%Y").strftime("%Y-%m-%d")
-            # remove HTML markup
-            br = row[4].replace("<br />", " ")
-            # convert value format
-            Amt = row[7].replace(".","")
-            Amt = Amt.replace(",",".")
-            # remove double spaces
-            RmtdNm = ' '.join(row[3].split())
-            RmtInf = ' '.join(br.split())
 
-            outputData.writerow(["", "", cur, OwnrAcctIBAN, OwnrAcctNo, OwnrAcctBIC, OwnrAcctBankCode, BookDt, ValDt, "", Amt, cur, "", row[10].strip(), "", row[9].strip(), row[8].strip(), RmtInf, "", row[2], "", "", "", RmtdNm, "", "", row[5], "", row[6], "", "", "", "", "", "", "", "", "false", "None"])
-            bookings +=1
+            # check if line starts with 2 date fields
+            if match("^\[('(\d{2}\.){2}\d{4}', ){2}", str(row)):
+                # convert booking date
+                BookDt = datetime.strptime(row[0], "%d.%m.%Y").strftime("%Y-%m-%d")
+                # convert value date
+                ValDt = datetime.strptime(row[1], "%d.%m.%Y").strftime("%Y-%m-%d")
+                # remove HTML markup
+                br = row[4].replace("<br />", " ")
+                # convert value format
+                Amt = row[7].replace(".","")
+                Amt = Amt.replace(",",".")
+                # remove double spaces
+                RmtdNm = ' '.join(row[3].split())
+                RmtInf = ' '.join(br.split())
+
+                outputData.writerow(["", "", cur, OwnrAcctIBAN, OwnrAcctNo, OwnrAcctBIC, OwnrAcctBankCode, BookDt, ValDt, "", Amt, cur, "", row[10].strip(), "", row[9].strip(), row[8].strip(), RmtInf, "", row[2], "", "", "", RmtdNm, "", "", row[5], "", row[6], "", "", "", "", "", "", "", "", "false", "None"])
+                # count converted lines
+                bookings +=1
 
 if not args.iban:
     print ("")
