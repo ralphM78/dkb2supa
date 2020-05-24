3195005d80372ed171cdf350f304ab982364b75c
ralphM78 <github-ralph@schosemail.de>
2020-05-24 23:06:20 +0200

    PEP8

--- dkb2supa.py
+++ dkb2supa.py
@@ -19,9 +19,9 @@
     Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
     veröffentlichten Version, weiter verteilen und/oder modifizieren.
 
-    Dieses Programm wird in der Hoffnung bereitgestellt, dass es nützlich sein wird, jedoch
-    OHNE JEDE GEWÄHR,; sogar ohne die implizite
-    Gewähr der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+    Dieses Programm wird in der Hoffnung bereitgestellt,
+    dass es nützlich sein wird, jedoch OHNE JEDE GEWÄHR,; sogar ohne die
+    implizite Gewähr der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
     Siehe die GNU General Public License für weitere Einzelheiten.
 
     Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
@@ -29,17 +29,42 @@
 '''
 
 
-import argparse, csv, os
+import argparse
+import csv
+import os
 from sys import exit
 from datetime import datetime
 from re import match
 from schwifty import IBAN
 
 parser = argparse.ArgumentParser(description="Convert DKB CSV into SUPA CSV.")
-parser.add_argument("-i", "--input", help="DKB CSV input file", type=argparse.FileType("r", encoding="iso-8859-1"), required=True, metavar="INPUTFILE")
-parser.add_argument("-o", "--output", help="SUPA formated CSV output file", type=argparse.FileType("w", encoding="utf-8"), required=True, metavar="OUTPUTFILE")
-parser.add_argument("--iban", help="Optionally your IBAN", metavar="DE820000000000")
-parser.add_argument("--cur", default="EUR", help="Optional Currency. Default is EUR", metavar="EUR")
+parser.add_argument(
+    "-i",
+    "--input",
+    help="DKB CSV input file",
+    type=argparse.FileType(
+        "r",
+        encoding="iso-8859-1"),
+    required=True,
+    metavar="INPUTFILE")
+parser.add_argument(
+    "-o",
+    "--output",
+    help="SUPA formated CSV output file",
+    type=argparse.FileType(
+        "w",
+        encoding="utf-8"),
+    required=True,
+    metavar="OUTPUTFILE")
+parser.add_argument(
+    "--iban",
+    help="Optionally your IBAN",
+    metavar="DE820000000000")
+parser.add_argument(
+    "--cur",
+    default="EUR",
+    help="Optional Currency. Default is EUR",
+    metavar="EUR")
 args = parser.parse_args()
 
 
@@ -82,18 +107,58 @@ with args.input as CsvInputfile:
     with args.output as CsvOutputfile:
         outputData = csv.writer(CsvOutputfile, delimiter=',')
         # writing header line
-        outputData.writerow(["Id", "AcctId", "OwnrAcctCcy", "OwnrAcctIBAN", "OwnrAcctNo", "OwnrAcctBIC", "OwnrAcctBankCode", "BookgDt", "ValDt", "TxDt", "Amt", "AmtCcy", "CdtDbtInd", "EndToEndId", "PmtInfId", "MndtId", "CdtrId", "RmtInf", "PurpCd", "BookgTxt", "PrimaNotaNo", "BankRef", "BkTxCd", "RmtdNm", "RmtdUltmtNm", "RmtdAcctCtry", "RmtdAcctIBAN", "RmtdAcctNo", "RmtdAcctBIC", "RmtdAcctBankCode", "BookgSts", "BtchBookg", "BtchId", "GVC", "GVCExtension", "Category", "Notes", "ReadStatus", "Flag"])
+        outputData.writerow(["Id",
+                             "AcctId",
+                             "OwnrAcctCcy",
+                             "OwnrAcctIBAN",
+                             "OwnrAcctNo",
+                             "OwnrAcctBIC",
+                             "OwnrAcctBankCode",
+                             "BookgDt",
+                             "ValDt",
+                             "TxDt",
+                             "Amt",
+                             "AmtCcy",
+                             "CdtDbtInd",
+                             "EndToEndId",
+                             "PmtInfId",
+                             "MndtId",
+                             "CdtrId",
+                             "RmtInf",
+                             "PurpCd",
+                             "BookgTxt",
+                             "PrimaNotaNo",
+                             "BankRef",
+                             "BkTxCd",
+                             "RmtdNm",
+                             "RmtdUltmtNm",
+                             "RmtdAcctCtry",
+                             "RmtdAcctIBAN",
+                             "RmtdAcctNo",
+                             "RmtdAcctBIC",
+                             "RmtdAcctBankCode",
+                             "BookgSts",
+                             "BtchBookg",
+                             "BtchId",
+                             "GVC",
+                             "GVCExtension",
+                             "Category",
+                             "Notes",
+                             "ReadStatus",
+                             "Flag"])
 
         bookings = 0
 
         for row in inputData:
 
             # check if line starts with 2 date fields
-            if match("^\[('(\d{2}\.){2}\d{4}', ){2}", str(row)):
+            if match(r"^\[('(\d{2}\.){2}\d{4}', ){2}", str(row)):
                 # convert booking date
-                BookDt = datetime.strptime(row[0], "%d.%m.%Y").strftime("%Y-%m-%d")
+                BookDt = datetime.strptime(
+                    row[0], "%d.%m.%Y").strftime("%Y-%m-%d")
                 # convert value date
-                ValDt = datetime.strptime(row[1], "%d.%m.%Y").strftime("%Y-%m-%d")
+                ValDt = datetime.strptime(
+                    row[1], "%d.%m.%Y").strftime("%Y-%m-%d")
                 # remove HTML markup
                 br = row[4].replace("<br />", " ")
                 # convert value format
@@ -103,7 +168,45 @@ with args.input as CsvInputfile:
                 RmtdNm = ' '.join(row[3].split())
                 RmtInf = ' '.join(br.split())
 
-                outputData.writerow(["", "", cur, OwnrAcctIBAN, OwnrAcctNo, OwnrAcctBIC, OwnrAcctBankCode, BookDt, ValDt, "", Amt, cur, "", row[10].strip(), "", row[9].strip(), row[8].strip(), RmtInf, "", row[2], "", "", "", RmtdNm, "", "", row[5], "", row[6], "", "", "", "", "", "", "", "", "false", "None"])
+                outputData.writerow(["",
+                                     "",
+                                     cur,
+                                     OwnrAcctIBAN,
+                                     OwnrAcctNo,
+                                     OwnrAcctBIC,
+                                     OwnrAcctBankCode,
+                                     BookDt,
+                                     ValDt,
+                                     "",
+                                     Amt,
+                                     cur,
+                                     "",
+                                     row[10].strip(),
+                                     "",
+                                     row[9].strip(),
+                                     row[8].strip(),
+                                     RmtInf,
+                                     "",
+                                     row[2],
+                                     "",
+                                     "",
+                                     "",
+                                     RmtdNm,
+                                     "",
+                                     "",
+                                     row[5],
+                                     "",
+                                     row[6],
+                                     "",
+                                     "",
+                                     "",
+                                     "",
+                                     "",
+                                     "",
+                                     "",
+                                     "",
+                                     "false",
+                                     "None"])
                 # count converted lines
                 bookings += 1
 
@@ -116,7 +219,19 @@ if args.iban:
     print("{:<25s} {:s}".format("Used account number is:", OwnrAcctNo))
 
 print("{:<25s} {:s}".format("Used currency:", cur))
-print("{:<25s} {:s}".format("Used input file:", os.path.dirname(os.path.realpath(__file__)) + "/" + args.input.name))
-print("{:<25s} {:s}".format("Used output file:", os.path.dirname(os.path.realpath(__file__)) + "/" + args.output.name))
+print(
+    "{:<25s} {:s}".format(
+        "Used input file:",
+        os.path.dirname(
+            os.path.realpath(__file__)) +
+        "/" +
+        args.input.name))
+print(
+    "{:<25s} {:s}".format(
+        "Used output file:",
+        os.path.dirname(
+            os.path.realpath(__file__)) +
+        "/" +
+        args.output.name))
 print("{:<25s} {:d}".format("Converted bookings:", bookings))
 print("")
